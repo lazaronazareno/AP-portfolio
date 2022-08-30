ea017245e4abf02179e55706319861a063489755
lazaronazareno <lazaronazareno@gmail.com>
2022-08-30 19:03:59 -0300

    form refactor

--- src/app/components/profile-form/profile-form.component.ts
+++ src/app/components/profile-form/profile-form.component.ts
@@ -14,11 +14,17 @@ export class ProfileFormComponent implements OnInit {
     this.form=this.formbuilder.group(
       {
         name:['', [Validators.required]],
-        profileImg:['', [Validators.required]],
-        backgroundImg:['', [Validators.required]],
-        position:['', [Validators.required]],
-        ubication:['', [Validators.required]],
-        about:['', [Validators.required]],
+        lastName:['', [Validators.required]],
+        nationality:['', [Validators.required]],
+        tel:['', [Validators.required]],
+        address:['', [Validators.required]],
+        mail:['', [Validators.required]],
+        about_me:['', [Validators.required]],
+        ocupation:['', [Validators.required]],
+        photo_url:['', [Validators.required]],
+        background_url:['', [Validators.required]],
+        repo_url:['', [Validators.required]],
+        linkedin_url:['', [Validators.required]],
       }
     )
    }
@@ -37,7 +43,7 @@ export class ProfileFormComponent implements OnInit {
     console.log(input.files?.item(0));
     this.imgbbService.uploadImg(img!).subscribe(url => (
       console.log(url.data.url),
-      this.form.value.profileImg = url.data.url 
+      this.form.value.photo_url = url.data.url 
     ))
 }
   handleBackgroundFileInput(e: Event) {
@@ -46,27 +52,45 @@ export class ProfileFormComponent implements OnInit {
     console.log(input.files?.item(0));
     this.imgbbService.uploadImg(img!).subscribe(url => (
       console.log(url.data.url),
-      this.form.value.backgroundImg = url.data.url 
+      this.form.value.background_url = url.data.url 
     ))
 }
 
   get Name(){
     return this.form.get('name');
   }
+  get LastName(){
+    return this.form.get('lastName');
+  }
+  get Nationality(){
+    return this.form.get('nationality');
+  }
+  get Tel(){
+    return this.form.get('tel');
+  }
+  get Address(){
+    return this.form.get('address');
+  }
+  get Mail(){
+    return this.form.get('mail');
+  }
+  get Ocupation(){
+    return this.form.get('ocupation');
+  }
   get ProfileImg(){
-    return this.form.get('profileImg');
+    return this.form.get('photo_url');
   }
   get BackgroundImg(){
-    return this.form.get('backgroundImg');
-  }
-  get Position(){
-    return this.form.get('position');
-  }
-  get Ubication(){
-    return this.form.get('ubication');
+    return this.form.get('background_url');
   }
   get About(){
-    return this.form.get('about');
+    return this.form.get('about_me');
+  }
+  get RepoUrl(){
+    return this.form.get('repo_url');
+  }
+  get LinkedinUrl(){
+    return this.form.get('linkedin_url');
   }
 
 }
