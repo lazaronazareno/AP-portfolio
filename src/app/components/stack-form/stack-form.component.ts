724ffc9d1057a94f1a7026b1b386a9b28f9f5952
lazaronazareno <lazaronazareno@gmail.com>
2022-10-21 12:51:42 -0300

    stack form loading and data response msgs

--- src/app/components/stack-form/stack-form.component.ts
+++ src/app/components/stack-form/stack-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { HtmlParser } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -13,8 +14,14 @@ import { UploadImgService } from 'src/app/services/upload-img.service';
 export class StackFormComponent implements OnInit {
   form:FormGroup;
   fileToUpload: File | null = null;
+
+  loading : boolean = false;
   response: string | undefined;
-  error : string | undefined;
+  error : HttpErrorResponse | undefined;
+
+  imgLoading : boolean = false;
+  imgResponse: string | undefined;
+  imgError : HttpErrorResponse | undefined;
 
   constructor(private formbuilder:FormBuilder, private imgbbService: UploadImgService,
      private portfolioService:PortfolioService, private route:Router) {
@@ -39,27 +46,39 @@ export class StackFormComponent implements OnInit {
 
   onSend(e:Event){
     e.preventDefault;
+    this.loading = true;
     console.log(this.form)
     this.portfolioService.postStack(this.form.value).subscribe({
       next : (data) => {
         console.log('post stack successfull', data);
         this.response = data;
+        this.loading = false;
         this.route.navigate(['/portfolio']);
       },
       error: (error) => {
         console.log('stack post failed', error);
         this.error = error;
+        this.loading = false;
       }
     })
   }
 
   handleFileInput(e: Event) {
+    this.imgLoading = true;
     const input = e.target as HTMLInputElement;
     const img = input.files?.item(0);
-    this.imgbbService.uploadImg(img!).subscribe(url => (
-      console.log(url.data.url),
-      this.form.value.photo_url = url.data.url 
-    ))
+    this.imgbbService.uploadImg(img as File).subscribe({
+      next: (url) => {
+        console.log(url.data.url),
+        this.form.value.photo_url = url.data.url 
+        this.imgResponse = `Imagen subida correctamente`
+        this.imgLoading = false;
+      },
+      error: (error) => {
+        this.imgError = error
+        this.imgLoading = false;
+      },
+    })
 }
 
 }
