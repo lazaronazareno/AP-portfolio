0f5109f2717c89042de6a712ab285900e5d0b4c1
lazaronazareno <lazaronazareno@gmail.com>
2022-10-25 21:42:58 -0300

    stack delete indicators

--- src/app/components/stack/stack.component.ts
+++ src/app/components/stack/stack.component.ts
@@ -2,6 +2,9 @@ import { Component, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from
 import { PortfolioService } from 'src/app/services/portfolio.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { ProfileComponent } from '../profile/profile.component';
+import { Stack } from 'src/app/porfolio-models';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-stack',
   templateUrl: './stack.component.html',
@@ -12,8 +15,11 @@ export class StackComponent implements OnInit {
   data:any;
   @Input() profileData:any;
   isUserAuth : boolean = false;
+  loading : boolean = false;
+  response: string | undefined;
+  error : HttpErrorResponse | undefined;
 
-  constructor(private portfolioService: PortfolioService, private authUser: AuthService) {
+  constructor(private portfolioService: PortfolioService, private authUser: AuthService, private router:Router) {
    }
 
   ngOnInit(): void {
@@ -22,6 +28,25 @@ export class StackComponent implements OnInit {
     .subscribe(data => (
       this.data = data
     ))
+  }
+
+  onDelete(stack : Stack) {
+    this.loading = true;
+    this.portfolioService.deleteStack(stack).subscribe({
+      next : (data) => {
+        console.log('stack deleted', data);
+        this.response = data;
+        this.loading = false
+        this.router.navigate(['/portfolio']).then(() => {
+          window.location.reload()
+        });
+      },
+      error: (error) => {
+        console.log('delete stack failed', error);
+        this.error = error;
+        this.loading = false
+      }
+    })
   }
 
 }
