29d68df830be40e930817d2626f8528eceae40aa
lazaronazareno <lazaronazareno@gmail.com>
2023-02-13 21:55:23 -0300

    changed backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   imgdbUrl: 'https://api.imgbb.com/1',
   imgdbKey: '60c12101d0e954516af4b57d07ac45cd',
-  backendUrl : 'https://argentina-programa-back-end.herokuapp.com'
+  backendUrl : 'https://argentina-programa-api-lazaronazareno.koyeb.app/'
 };
